fd5b015d3614634f88cd8721c35383a372fd7af8
soaeng <sodud0695@naver.com>
2022-05-21 13:24:55 +0900

    생성, 설정, bootstrap

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        document.getElementById('sidebar').classList.remove('active');
+    }
+});
+window.addEventListener('resize', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        document.getElementById('sidebar').classList.remove('active');
+    }else{
+        document.getElementById('sidebar').classList.add('active');
+    }
+});
